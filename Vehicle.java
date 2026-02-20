8712ab9c4da0a981b2dd0993833b30c152aa212c
mutungu23606-a11y <mutungu.23606@students.kyu.ac.ke>
2026-02-20 21:06:30 +0300

    Create Vehicle.java

---
+++ Vehicle.java
@@ -0,0 +1,20 @@
+// Vehicle.java
+class Vehicle {
+    String brand;
+    String model;
+    int year;
+
+    // Constructor to initialize fields
+    public Vehicle(String brand, String model, int year) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+    }
+
+    // Method to display vehicle details
+    public void displayDetails() {
+        System.out.println("Brand: " + brand);
+        System.out.println("Model: " + model);
+        System.out.println("Year: " + year);
+    }
+}
